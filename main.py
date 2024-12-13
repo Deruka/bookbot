40cf0371e432b2f464b26743005aab0081ddd98c
Deruka <deruka.km@googlemail.com>
2024-12-13 17:07:22 +0100

    main.py added function count_characters

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ def main():
         file_contents = f.read()
         print(file_contents)
         amount_words = count_words(file_contents)
+        amount_characters = count_characters(file_contents)
         print(f"the book contains {amount_words} words")
+        print(amount_characters)
 
 def count_words(file_to_count):
     words = file_to_count.split()
     count = len(words)
     return count
+
+def count_characters(file_to_count):
+    characters = {}
+    words_lower = file_to_count.lower()
+    for character in words_lower:
+        if character.isalpha():
+            if character in characters:
+                characters[character] += 1
+            else:
+                characters[character] = 1
+    return characters
 
 main()
