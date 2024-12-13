f816fb1ac6db8b13739089136367b5f50c460ab3
Deruka <deruka.km@googlemail.com>
2024-12-13 17:33:20 +0100

    main.py modified - it now prints out a sorted report

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    path_to_file = "books/frankenstein.txt"
+    with open(path_to_file) as f:
         file_contents = f.read()
-        print(file_contents)
         amount_words = count_words(file_contents)
         amount_characters = count_characters(file_contents)
-        print(f"the book contains {amount_words} words")
-        print(amount_characters)
+        list_of_dicts = [{'character': key, 'num': value} for key, value in amount_characters.items()]
+        list_of_dicts.sort(reverse=True, key=sort_on)
+        # Report Print
+        print(f"--- Begin report of {path_to_file} ---")
+        print(f"{amount_words} words found in the document")
+        print(" ")
+        for item in list_of_dicts:
+            print(f"The '{item['character']}' character was found {item['num']} times")
+        print("--- End report ---")
 
 def count_words(file_to_count):
     words = file_to_count.split()
@@ -22,5 +29,8 @@ def count_characters(file_to_count):
             else:
                 characters[character] = 1
     return characters
+
+def sort_on(dict):
+    return dict["num"]
 
 main()
